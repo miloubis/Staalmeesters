1cded3cf8cc3d6090dc853064d86e0e7117b38de
LeoDAB <31652036+LeoDAB@users.noreply.github.com>
2017-11-14 14:13:59 +0100

    .

--- Code/bestfit.py
+++ Code/bestfit.py
@@ -2,4 +2,10 @@ import nunpy as np
 from helper import *
 from load_orders import *
 
+# create an empty array
+order1 = order1.orderlist
+maxLengthRoll = order1.maxLengthRoll / 10
+rollC = np.zeros((maxLengthRoll,55))
+
+
 def pack(bestFit, )
