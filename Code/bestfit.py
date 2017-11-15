58ff1155e78ffcbcedd25454f294fa50b95b50e7
Morena Bastiaansen <mbastiaansen@hotmail.com>
2017-11-15 12:48:47 +0100

    .

--- Code/bestfit.py
+++ Code/bestfit.py
@@ -2,12 +2,17 @@ import nunpy as np
 from helper import *
 from load_orders import *
 
+def search_skyline(rollC, ):
+	
+
+
 # create an empty array
 order1 = order1.orderlist
 maxLengthRoll = order1.maxLengthRoll / 10
 rollC = np.zeros((maxLengthRoll,55))
 
+possibleWidth = 55-
 
-
-
-def pack(order)
+def pack(bestFit, rollC):
+	skyline_width=rollC.size
+	rollC=np.append(rollC(), bestFit, axis=1)
