dad5bf63dbf5f9b33a281c2b977095420d31bd48
miloubis <milou_bisseling@hotmail.com>
2017-11-22 16:07:29 +0100

    Update code

--- Code/bottomleft.py
+++ Code/bottomleft.py
@@ -2,14 +2,30 @@ from helper import *
 from load_orders import *
 import numpy as np
 
-# define order, list of remaining orders and order number
+# load orderlist
 orderlist = order1.orderlist
-remainingOrders = orderlist
-orderNum = 0
 
 # create grid for steel roll
 maxLengthRoll = int(order1.maxLengthRoll/10)
 widthRoll = int(ROLL_C/10)
 rollC = np.zeros((maxLengthRoll,widthRoll))
 
+# divide orderlist by 10
+dividedOrderlist = []
+for i in range(len(orderlist)):
+    order1 = int(orderlist[i][0] / 10)
+    order2 = int(orderlist[i][1] / 10)
+    dividedOrderlist.append([order1,order2])
+
+print(dividedOrderlist)
+
 # search for empty space
+# print(rollC.shape[1])
+
+# iterate through orderlist
+# for i in range(len(orderlist)):
+#     print(orderlist[i])
+
+# length = rollC.shape[0]
+# width = rollC.shape[1]
+
