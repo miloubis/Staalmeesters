00fb030205708276ccb7d154af09bf782577c4bd
miloubis <milou_bisseling@hotmail.com>
2017-11-27 14:12:52 +0100

    Merge files

--- Code/numpy_test.py
+++ Code/numpy_test.py
@@ -4,11 +4,11 @@ orders = [[2,3],[2,2],[2,1],[1,3],[1,4]]
 
 roll = np.zeros((48,5))
 
-# roll[0:2, 0:3] = 1
-# roll[2:4, 0:2] = 2
-# roll[4:6, 0:1] = 3
-# roll[6:7, 0:3] = 4
-# roll[7:8, 0:4] = 5
+roll[0:2, 0:3] = 1
+roll[2:4, 0:2] = 2
+roll[4:6, 0:1] = 3
+roll[6:7, 0:3] = 4
+roll[7:8, 0:4] = 5
 
 column_pos = 0
 row_pos = 0
@@ -24,18 +24,18 @@ row_pos = 0
 # roll[row_pos:row_pos + orders[4][0], column_pos:orders[4][1]] = 5
 # row_pos += orders[4][0]
 
-<<<<<<< HEAD
-for i in range(1,len(orders)):
-    roll[row_pos:row_pos + orders[i-1][0], column_pos:orders[i][1]] = i
-    print ("--------------------------{}--------------------------".format(column_pos))
-    row_pos += orders[i][0]
-    print ("--------------------------{}--------------------------".format(row_pos))
-    # print(i[0])
-=======
-for i in range(1,len(orders)+1):
-    roll[row_pos:row_pos + orders[i - 1][0], column_pos:orders[i - 1][1]] = i
-    row_pos += orders[i - 1][0]
->>>>>>> eef2d4ff235902c25204e7473421c27ad6736710
+#
+# for i in range(1,len(orders)):
+#     roll[row_pos:row_pos + orders[i-1][0], column_pos:orders[i][1]] = i
+#     print ("--------------------------{}--------------------------".format(column_pos))
+#     row_pos += orders[i][0]
+#     print ("--------------------------{}--------------------------".format(row_pos))
+#     # print(i[0])
+#
+# for i in range(1,len(orders)+1):
+#     roll[row_pos:row_pos + orders[i - 1][0], column_pos:orders[i - 1][1]] = i
+#     row_pos += orders[i - 1][0]
+# >>>>>>> eef2d4ff235902c25204e7473421c27ad6736710
 
 
 # for x in np.nditer(roll, op_flags=['readwrite']):
@@ -45,4 +45,4 @@ for i in range(1,len(orders)+1):
          # for i in orderlist:
          #    roll[0:orderlist[0][0], 0:orderlist[0][1]] = 1
 
-# print(roll)
+print(roll)
