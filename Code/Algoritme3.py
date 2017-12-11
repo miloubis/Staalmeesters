707bddb6cf955561a6f5f015ae823d0c48a14d4f
Morena Bastiaansen <mbastiaansen@hotmail.com>
2017-12-11 13:35:51 +0100

    .

--- Code/Algoritme3.py
+++ Code/Algoritme3.py
@@ -12,9 +12,34 @@ orderNum = 0
 rollC = np.zeros((order1.maxLengthRoll, ROLL_C))
 
 # Place sub orders from an order randomly for 500 times
-for i in range(500):
+	row = 0
+  	col = 0
     random.shuffle(remainingOrders)
+    for j in range(len(remainingOrders)):
+    	subOrder = remainingOrders[j]
+    	for n in range(subOrder[0]):
+    		for m in range(subOrder[1]):
+    			if subOrder[0] <= 55 - row:
+    				rollC[row + n][col + m] = orderNum*100
+    				col += m
+    			elif subOrder[0] >= 55 - row:
+    				col = 0
+  					row += height 
+    				rollC[row + n][col + m] = orderNum*100
+    	height = subOrder[1]
 
-    # Place all suborders
-    while remainingOrders:
+
+
+    	(rollC)
+
+
+
+
+
+
+
+
+
+
+
 
