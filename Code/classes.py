c5164c22b605ecf66920be3ba8a914675f4c7c32
LeoDAB <31652036+LeoDAB@users.noreply.github.com>
2017-12-15 13:07:36 +0100

    comment update

--- Code/classes.py
+++ Code/classes.py
@@ -1,15 +1,25 @@
 import copy
 # Create order class with useful attributes
 class Order:
-    """" In this class all useful attributes for an order are saved """
+    """"
+    In this class all useful attributes for an order are saved .
+    """
 
     def __init__(self, orderlist):
+        """
+        This funtion initializes the order attributes.
+        :param orderlist: List of sub orders.
+        """
         self.orderlist = orderlist
         self.totalArea = self.area(orderlist)
         self.maxLengthRoll = self.max_length(orderlist)
-        #self.area = area(orderlist)
 
     def area(self, orderlist):
+        """
+        This function calculates the total surface area of all sub orders in an order.
+        :param orderlist: List of sub orders.
+        :return: The total area of all sub orders combined.
+        """
         totalArea = 0
         if len(orderlist[0]) == 2 or isinstance(orderlist[0][2], str):
             for i in range(len(orderlist)):
@@ -17,6 +27,12 @@ class Order:
         return totalArea
 
     def max_length(self, orderlist):
+        """
+        This function calculates the maximum length of steel roll needed.
+        This maximum length is based on placing all suborders on top of each other with longest side as rows.
+        :param orderlist: List of sub orders.
+        :return: The maximum length of steel roll need for an order.
+        """
         maxLength = 0
         for i in range(len(orderlist)):
             maxLength += max(orderlist[i][0], orderlist[i][1])
